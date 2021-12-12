5410e2ee883b6b86cbdae122a705168b9c404754
NaimAsSakib <rifat.sakib90@gmail.com>
2021-12-12 20:08:17 +0600

    redirected to wikipedia when a country is clicked

--- app/src/main/java/com/example/recyclerview5/MainActivity2.java
+++ app/src/main/java/com/example/recyclerview5/MainActivity2.java
@@ -2,11 +2,15 @@ package com.example.recyclerview5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     TextView textViewC_Name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,16 @@ public class MainActivity2 extends AppCompatActivity {
         textViewC_Name=findViewById(R.id.tv_Show_C_Name);
 
         textViewC_Name.setText(getIntent().getStringExtra("passCountryName"));
+
+
+        textViewC_Name.setOnClickListener(new View.OnClickListener() {  //to redirect to wikipedia when country is clicked
+            @Override
+            public void onClick(View v) {
+                String url="https://en.wikipedia.org/wiki/"+textViewC_Name.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
